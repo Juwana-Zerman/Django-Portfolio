efefb7fb3970bf1ea8a45f91ef82536e006cfea0
Juwana Zerman <juwanazerman@gmail.com>
2022-12-29 16:11:57 -0500

    Update forms.py

--- home/forms.py
+++ home/forms.py
@@ -1,7 +1,7 @@
 from django.forms import ModelForm
-from django.forms import Textarea, EmailField, CharField
-from .models import Contact
-
+from django.forms import Textarea, EmailField, CharField, ImageField
+from .models import Contact, Post
+from django import forms
 class ContactForm(ModelForm):
     class Meta:
         model = Contact
@@ -13,4 +13,39 @@ class ContactForm(ModelForm):
             "message": Textarea(attrs={
                 "placeholder": "Send me a message"
             })
+        }
+
+class PostForm(ModelForm):
+    class Meta:
+        model = Post
+        fields = [
+            'title',
+            'slug',
+            'author',
+            'content',
+            'status',
+            'image',
+            'published_date']
+        widgets = {
+            "title": Textarea(attrs={
+                "placeholder": "Title"
+            }),
+            "slug": Textarea(attrs={
+                "placeholder": "Slug"
+            }),
+            "author": Textarea(attrs={
+                "placeholder": "Author"
+            }),
+            "content": Textarea(attrs={
+                "placeholder": "Content"
+            }),
+            "status": Textarea(attrs={
+                "placeholder": "Status"
+            }),
+            "image": Textarea(attrs={
+                "placeholder": "Image"
+            }),
+            "published_date": Textarea(attrs={
+                "placeholder": "Published Date"
+            }),
         }
